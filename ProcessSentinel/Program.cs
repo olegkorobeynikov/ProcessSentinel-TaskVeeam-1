4e281442b83ece6146546f32159b035c797853ef
Oleg Korobeinikov <korobeinikov@skbkontur.ru>
2022-05-10 12:18:50 +0200

    pull miss programm.cs

---
+++ ProcessSentinel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProcessSentinel
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Command command = null;
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Enter command end press Enter.");
+            }
+            else
+            {
+                CommandParser.TryParse(args, out command);
+            }
+
+            command = command != null && command.IsSet() ? command : CommandReader.Read();
+            
+            var processSentinel = new ProcessSentinel(command);
+            processSentinel.Start();
+        }
+    }
+}
